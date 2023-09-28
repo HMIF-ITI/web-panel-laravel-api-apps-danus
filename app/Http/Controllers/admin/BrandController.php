23b213346cd1b36039b33bf75f3ce1403e7a654d
DESKTOP-JBVEJL5\apipu <apipumar39@gmail.com>
2023-09-29 05:59:15 +0700

    MVC create brands

---
+++ app/Http/Controllers/admin/BrandController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BrandController extends Controller
+{
+    public function create() {
+        return view('admin.brands.create');
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'slug' => 'required|unique:brands',
+        ]);
+
+        if ($validator->passes()) {
+            $brand = new Brand();
+            $brand->name = $request->name;
+            $brand->slug = $request->slug;
+            $brand->status = $request->status;
+            $brand->save();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Brand added successfully'
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'erorrs' => $validator->errors()
+            ]);
+        }
+    }
+}
