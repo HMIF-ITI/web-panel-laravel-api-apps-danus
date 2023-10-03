ac1861a3ea26f131bd66bd3e5e5f68a32e108912
Umar Khoirul Afif <110627263+Umar29072000@users.noreply.github.com>
2023-10-04 06:47:33 +0700

    resolving bugs

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -39,8 +39,8 @@
                         </div>
                         <div class="col-md-6">
                             <div class="mb-3">
+                                <label for="image_id">Image</label>
                                 <input type="hidden" id="image_id" name="image_id" value="">
-                                <label for="image">Image</label>
                                 <div id="image" class="dropzone dz-clickable">
                                     <div class="dz-message needsclick">    
                                         <br>Drop files here or click to upload.<br><br>                                            
