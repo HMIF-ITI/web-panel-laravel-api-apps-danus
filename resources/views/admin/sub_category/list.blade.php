06af5e429ca25e3c8b367802835c6a07fcb6e567
Umar Khoirul Afif <110627263+Umar29072000@users.noreply.github.com>
2023-10-04 06:45:52 +0700

    Fixing bugsss

--- resources/views/admin/sub_category/list.blade.php
+++ resources/views/admin/sub_category/list.blade.php
@@ -118,10 +118,9 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 },
                 success: function(response) {
-                    window.location.href="{{ route('sub-categories.index') }}";
-                    // if (response["status"]) {
-                    //     window.location.href="{{ route('sub-categories.index') }}";
-                    // }
+                    if (response["status"]) {
+                        window.location.href="{{ route('sub-categories.index') }}";
+                    }
                 }
             });
         }
