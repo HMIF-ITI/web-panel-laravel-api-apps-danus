ee5b13963ceecedb7a861638952ccf7547f62328
Umar Khoirul Afif <110627263+Umar29072000@users.noreply.github.com>
2023-10-04 06:42:21 +0700

    Fixing bugs

--- database/migrations/2023_09_19_194937_create_categories_table.php
+++ database/migrations/2023_09_19_194937_create_categories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug');
-            $table->string('image')->default(NULL);
+            $table->string('image')->nullable();
             $table->integer('status')->default(1);
             $table->timestamps();
         });
